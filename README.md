e6f5339612553b39038e8382441d95922ec7b5b6
samuelColon <samsoncs@gmail.com>
2015-05-16 20:21:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+An instagram'esque Rails app.
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  devise_for :users
+  
+
+  resources :posts do 
+  	member do
+  	  get "like", to: "posts#upvote"
+  	  get "dislike", to: "posts#downvote"
+  	end
+  	resources :comments
+  end
+  
+  root 'posts#index'
+  match ':controller/:action(/:id)', :via => [:get, :post]
+end
